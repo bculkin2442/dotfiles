340665dd86270620905cc975dc4299e206b08485
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2018-01-11 12:48:29 -0400

    Add mvn settings

--- restow.sh
+++ restow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Restow.sh - Add all new files to their stowed locations
 
-for dir in astyle, bash, dillo, dunst, git, idesk, mutt, screen, tmux, vim, x, xdock; do
+for dir in astyle, bash, dillo, dunst, git, idesk, mvn, mutt, screen, tmux, vim, x, xdock; do
 	xstow dir
 done
